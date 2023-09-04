7ff1fe299037462e7f9dc2540ecd76e5d5af3fe7
sergega-fan <sergei_nikitin00@mail.ru>
2023-09-04 07:23:54 +0300

    Table screen almost done

--- src/pages/MainPage/ui/TableScreen/TableScreen.tsx
+++ src/pages/MainPage/ui/TableScreen/TableScreen.tsx
@@ -10,7 +10,74 @@ export const TableScreen = (props: TableScreenProps) => {
 
   return (
     <div className={cn(className, cls.TableScreen)}>
-      table screen
+      <div className={cn(cls.Table)}>
+        <div className={cn(cls.TableRow, cls.TableRowHeader)}>
+          <div className={cn(cls.TableRowLeft, cls.TableRowSide)}>
+
+          </div>
+          <div className={cn(cls.TableRowMiddle, cls.TableRowSide)}>
+            Отборочный этап
+          </div>
+          <div className={cn(cls.TableRowRight, cls.TableRowSide)}>
+            Заключительный этап
+          </div>
+        </div>
+        <div className={cn(cls.TableRow)}>
+          <div className={cn(cls.TableRowLeft, cls.TableRowSide)}>
+            Криптография
+          </div>
+          <div className={cn(cls.TableRowMiddle, cls.TableRowSide)}>
+            с 01 по 20 ноября 2022 г.
+          </div>
+          <div className={cn(cls.TableRowRight, cls.TableRowSide)}>
+            27 ноября 2022 г.
+          </div>
+        </div>
+        <div className={cn(cls.TableRow)}>
+          <div className={cn(cls.TableRowLeft, cls.TableRowSide)}>
+            Криптография
+          </div>
+          <div className={cn(cls.TableRowMiddle, cls.TableRowSide)}>
+            с 01 по 20 ноября 2022 г.
+          </div>
+          <div className={cn(cls.TableRowRight, cls.TableRowSide)}>
+            27 ноября 2022 г.
+          </div>
+        </div>
+        <div className={cn(cls.TableRow)}>
+          <div className={cn(cls.TableRowLeft, cls.TableRowSide)}>
+            Криптография
+          </div>
+          <div className={cn(cls.TableRowMiddle, cls.TableRowSide)}>
+            с 01 по 20 ноября 2022 г.
+          </div>
+          <div className={cn(cls.TableRowRight, cls.TableRowSide)}>
+            27 ноября 2022 г.
+          </div>
+        </div>
+        <div className={cn(cls.TableRow)}>
+          <div className={cn(cls.TableRowLeft, cls.TableRowSide)}>
+            Криптография
+          </div>
+          <div className={cn(cls.TableRowMiddle, cls.TableRowSide)}>
+            с 01 по 20 ноября 2022 г.
+          </div>
+          <div className={cn(cls.TableRowRight, cls.TableRowSide)}>
+            27 ноября 2022 г.
+          </div>
+        </div>
+        <div className={cn(cls.TableRow)}>
+          <div className={cn(cls.TableRowLeft, cls.TableRowSide)}>
+            Криптография
+          </div>
+          <div className={cn(cls.TableRowMiddle, cls.TableRowSide)}>
+            с 01 по 20 ноября 2022 г.
+          </div>
+          <div className={cn(cls.TableRowRight, cls.TableRowSide)}>
+            27 ноября 2022 г.
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
