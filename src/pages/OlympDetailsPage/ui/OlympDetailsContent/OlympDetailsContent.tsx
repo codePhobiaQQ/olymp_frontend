97c727540d613d55165c24a966e37db9594ea40a
codePhobiaQQ <vital.pereg@mail.ru>
2023-09-05 14:21:17 +0300

    Done switching active menu items

--- src/pages/OlympDetailsPage/ui/OlympDetailsContent/OlympDetailsContent.tsx
+++ src/pages/OlympDetailsPage/ui/OlympDetailsContent/OlympDetailsContent.tsx
@@ -1,5 +1,8 @@
 import cn from 'classnames'
 import cls from './OlympDetailsContent.module.scss'
+import { useSelector } from 'react-redux'
+import { getActiveMenuItem } from './../../model/selectors/olympDetailsSelectors.ts'
+import { OlympDetailsMenuItems } from '@pages/OlympDetailsPage/model/types/olympsDetails.ts'
 
 type OlympDetailsContentProps = {
   className?: string
@@ -7,6 +10,28 @@ type OlympDetailsContentProps = {
 
 export const OlympDetailsContent = (props: OlympDetailsContentProps) => {
   const { className = '' } = props
+  const activeMenuItem = useSelector(getActiveMenuItem)
 
-  return <div className={cn(className, cls.OlympDetailsContent)}></div>
+  let content
+  if (activeMenuItem === OlympDetailsMenuItems.NEWS) {
+    content = 'NEWS'
+  } else if (activeMenuItem === OlympDetailsMenuItems.OF_INFO) {
+    content = 'OF_INFO'
+  } else if (activeMenuItem === OlympDetailsMenuItems.TIME_PLACE) {
+    content = 'TIME_PLACE'
+  } else if (activeMenuItem === OlympDetailsMenuItems.ARCHIVE) {
+    content = 'ARCHIVE'
+  } else if (activeMenuItem === OlympDetailsMenuItems.PREV_TASKS) {
+    content = 'PREV_TASKS'
+  } else if (activeMenuItem === OlympDetailsMenuItems.PREPARE) {
+    content = 'PREPARE'
+  } else if (activeMenuItem === OlympDetailsMenuItems.RESULTS) {
+    content = 'RESULTS'
+  } else if (activeMenuItem === OlympDetailsMenuItems.PARTNERS) {
+    content = 'PARTNERS'
+  } else if (activeMenuItem === OlympDetailsMenuItems.ABOUT) {
+    content = 'ABOUT'
+  }
+
+  return <div className={cn(className, cls.OlympDetailsContent)}>{content}</div>
 }
