d1cc1ea8f86eb68dd7539a6072175d5e48566792
codePhobiaQQ <vital.pereg@mail.ru>
2023-09-13 10:37:33 +0300

    Done fixing styles for presentation screen

--- src/pages/MainPage/ui/PresentationScreen/PresentationScreen.tsx
+++ src/pages/MainPage/ui/PresentationScreen/PresentationScreen.tsx
@@ -2,15 +2,20 @@ import cn from 'classnames'
 import cls from './PresentationScreen.module.scss'
 import { useSelector } from 'react-redux'
 import {
-  getPresentationButtonText1, getPresentationButtonText2,
+  getPresentationButtonText1,
+  getPresentationButtonText2,
   getPresentationScreenNumb1,
-  getPresentationScreenNumb2, getPresentationText1, getPresentationText2,
+  getPresentationScreenNumb2,
+  getPresentationText1,
+  getPresentationText2,
 } from '@pages/MainPage/model/selectors/mainPageSelectors'
 import Button from '@shared/ui/Button/Button'
+import { HStack, VStack } from '@shared/ui/Stack'
+import { WpTextFormatter } from '@shared/ui/WpTextFormatter/WpTextFormatter.tsx'
 
 type PresentationScreenProps = {
-  className?: string;
-};
+  className?: string
+}
 
 export const PresentationScreen = (props: PresentationScreenProps) => {
   const { className } = props
@@ -25,31 +30,33 @@ export const PresentationScreen = (props: PresentationScreenProps) => {
     <section className={cn(className, cls.PresentationScreen)}>
       <div className={cn(cls.DecorLine)}></div>
 
-      <div className={cn('container', 'flex', 'items-center', 'justify-between')}>
-        <div className={cn(cls.NumberSide)}>
-          {numb1}
-        </div>
-        <div className={cn(cls.RightSide)}>
-          <div className={cn(cls.RightSideInner)}>
-            {text1}
-            <Button variant={'outline_transparent_arrow'}>{buttonText1}</Button>
-          </div>
-        </div>
-      </div>
+      <HStack align="center" justify="between" className={cn('container')}>
+        <div className={cn(cls.NumberSide)}>{numb1}</div>
+
+        <VStack className={cn(cls.RightSide)}>
+          <VStack gap="16" className={cn(cls.RightSideInner)}>
+            <WpTextFormatter className={cn(cls.Content)} content={text1} />
+            <Button className={cn(cls.PresentationBtn)} variant="outline_transparent_arrow">
+              {buttonText1}
+            </Button>
+          </VStack>
+        </VStack>
+      </HStack>
 
       <div className={cn(cls.DecorLine)}></div>
 
-      <div className={cn('container', 'flex', 'items-center', 'justify-between')}>
-        <div className={cn(cls.NumberSide)}>
-          {numb2}
-        </div>
-        <div className={cn(cls.RightSide)}>
-          <div className={cn(cls.RightSideInner)}>
-            {text2}
-            <Button variant={'outline_transparent_arrow'}>{buttonText2}</Button>
-          </div>
-        </div>
-      </div>
+      <HStack align="center" justify="between" className={cn('container')}>
+        <div className={cn(cls.NumberSide)}>{numb2}</div>
+
+        <VStack className={cn(cls.RightSide)}>
+          <VStack gap="16" className={cn(cls.RightSideInner)}>
+            <WpTextFormatter className={cn(cls.Content)} content={text2} />
+            <Button className={cn(cls.PresentationBtn)} variant="outline_transparent_arrow">
+              {buttonText2}
+            </Button>
+          </VStack>
+        </VStack>
+      </HStack>
 
       <div className={cn(cls.DecorLine)}></div>
     </section>
