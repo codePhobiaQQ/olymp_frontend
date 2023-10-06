a134c3e6f4ee23d6e6ee73cac66f348903642a57
codePhobiaQQ <vital.pereg@mail.ru>
2023-10-06 09:39:22 +0300

    Add add variable

--- src/shared/vars/vars.ts
+++ src/shared/vars/vars.ts
@@ -1,1 +1,2 @@
-export const __API__ = 'https://olymp.ivanskanavi.com/repo/wp-json'
+// export const __API__ = 'https://olymp.ivanskanavi.com/repo/wp-json'
+export const __API__ = import.meta.env.VITE_API
